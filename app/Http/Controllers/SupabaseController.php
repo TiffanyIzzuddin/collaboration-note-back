d1b61147653001952233d6c2e75d3541986d0e41
rizkysp6162 <rizkysp6162@gmail.com>
2023-11-03 19:30:11 +0700

    e

--- app/Http/Controllers/SupabaseController.php
+++ app/Http/Controllers/SupabaseController.php
@@ -12,9 +12,27 @@ class SupabaseController extends Controller
     {
         $supabase = new Supabase('https://emjzqwzrslcmwtefrffi.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtanpxd3pyc2xjbXd0ZWZyZmZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgzMjY0OTEsImV4cCI6MjAxMzkwMjQ5MX0.HvyAmsJFGwhaMaB-d0jBIf-07lNM6IiulKPPSrhl1sE');
         $data = $supabase->getData('worksheet');
-        dd($data);
-        return view('index', compact($data));
+        
+        return view('index', compact('data'));
     }
 
+    public function getData()
+    {
+        $supabaseUrl = 'https://emjzqwzrslcmwtefrffi.supabase.co';
+        $supabaseApiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtanpxd3pyc2xjbXd0ZWZyZmZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgzMjY0OTEsImV4cCI6MjAxMzkwMjQ5MX0.HvyAmsJFGwhaMaB-d0jBIf-07lNM6IiulKPPSrhl1sE';
+
+        $supabase = new Supabase($supabaseUrl, $supabaseApiKey);
+
+        $response = $supabase->from('worksheet')->select('*')->execute();
+
+        if ($response->error) {
+            // Penanganan kesalahan
+            return 'error';
+        } else {
+            $data = $response->data;
+            // Proses data
+            return view('index','data');
+        }
+    }
     // Tambahkan fungsi lain yang Anda butuhkan di sini.
 }
--- app/Utils/Supabase.php
+++ app/Utils/Supabase.php
@@ -6,15 +6,16 @@ use GuzzleHttp\Client;
 
 class Supabase
 {
-    private $url;
+    protected $selectedColumns;
     private $apiKey;
     private $client;
+    protected $table;
+    protected $baseUrl;
 
-    public function __construct($url, $apiKey)
+    public function __construct($baseUrl, $apiKey)
     {
-        $this->url = $url;
+        $this->baseUrl = $baseUrl;
         $this->apiKey = $apiKey;
-        $this->client = new Client(['base_uri' => $this->url]);
     }
 
     public function getData($table)
@@ -28,6 +29,36 @@ class Supabase
 
         return json_decode($response->getBody(), true);
     }
+    public function from($table)
+    {
+        $this->table = $table;
+        return $this;
+    }
 
+    public function select($columns)
+    {
+    $this->selectedColumns = $columns; // Simpan daftar kolom yang dipilih
+    return $this;
+    }
+
+    public function execute()
+    {
+        // Implementasi metode execute() akan mengirimkan permintaan HTTP sesuai dengan data yang telah ditentukan
+        $url = $this->baseUrl . '/rest/v1/' . $this->table;
+        
+        // Buat permintaan HTTP menggunakan Guzzle atau klien HTTP lainnya
+        $client = new Client();
+
+        $response = $client->request('GET', $url, [
+            'headers' => [
+                'apikey' => $this->apiKey,
+                'Content-Type' => 'application/json',
+            ],
+        ]);
+
+        // Anda dapat memproses respons dan mengembalikan data yang diambil dari Supabase
+        $data = json_decode($response->getBody(), true);
+        return $data;
+    }
     // Tambahkan metode lain yang Anda perlukan di sini.
 }
